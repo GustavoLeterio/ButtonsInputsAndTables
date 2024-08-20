09018bf3ebfcea2fbd1d53ad80910405c1e8d2c4
GustavoLeterio <gustavoleterio@gmail.com>
2024-08-20 18:01:25 -0300

    movendo componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { StageListComponent } from './../Components/stage-list/stage-list.component';
-import { TextareaComponent } from './../Components/textarea/textarea.component';
-import { InfoBlockComponent } from './../Components/info-block/info-block.component';
-import { TableComponent } from './../Components/table/table.component';
-import { ProgressBarComponent } from './../Components/progress-bar/progress-bar.component';
-import { SelectComponent } from './../Components/select/select.component';
-import { InputComponent } from './../Components/input/input.component';
-import { TitleComponent } from './../Components/title/title.component';
-import { SidebarComponent } from './../Components/sidebar/sidebar.component';
-import { ButtonComponent } from './../Components/button/button.component';
+import { StageListComponent } from './Components/stage-list/stage-list.component';
+import { TextareaComponent } from './Components/textarea/textarea.component';
+import { InfoBlockComponent } from './Components/info-block/info-block.component';
+import { TableComponent } from './Components/table/table.component';
+import { ProgressBarComponent } from './Components/progress-bar/progress-bar.component';
+import { SelectComponent } from './Components/select/select.component';
+import { InputComponent } from './Components/input/input.component';
+import { TitleComponent } from './Components/title/title.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { ButtonComponent } from './Components/button/button.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
